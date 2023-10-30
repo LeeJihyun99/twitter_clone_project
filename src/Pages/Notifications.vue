6f829e5eddcf44e30991df55fcb5d2853fa2c8af
ljihyun99 <Ji.Lee@campus.lmu.de>
2023-10-30 23:54:47 +0100

    added scrollbar to the notification section

--- src/Pages/Notifications.vue
+++ src/Pages/Notifications.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex-1 border-r border-gray">
+    <div class="flex-1 border-r border-gray overflow-y-auto no-scrollbar">
         <div class="flex flex-col">
             <!-- title section -->
             <div class="flex flex-row justify-between mt-4 mb-4 items-center">
@@ -13,7 +13,7 @@
                 <div class="hover:bg-gray hover:bg-opacity-80 w-1/3 pt-4 hover:border-b-4 hover:border-twitteronMouse cursor-pointer">Mentions</div>
             </div>
             <!-- notifications list -->
-            <div class="flex flex col" v-for="notification in 3">
+            <div class="flex flex col" v-for="notification in 20">
                 <div class="flex border-b border-gray mx-4 space-x-4 px-2 py-2 hover:bg-gray cursor-pointer">
                     <img src="http://picsum.photos/50" class="hover:brightness-75 w-14 h-14 rounded-full "/>
                     <span class="font-semibold">There was a login to your account @JihyunLee477145 from a new device on 2023년 10월 29일. Review it now.</span>
