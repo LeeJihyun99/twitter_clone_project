7d4229c19d2a8c2a693d8c9b5247954cc89bd796
ljihyun99 <Ji.Lee@campus.lmu.de>
2023-10-31 16:11:01 +0100

    chatting section (HTML+CSS) done

--- src/Pages/Messages.vue
+++ src/Pages/Messages.vue
@@ -33,7 +33,75 @@
         </div>
     </div>
     <!-- chat -->
-    <div class="w-3/5">b</div>
+    <div class="w-3/5 border-gray border-r ">
+        <div class="flex flex-col h-screen overflow-y-auto">
+            <!-- profile on top  -->
+            <div class="flex border-b border-gray justify-between p-3">
+                <div class=flex>
+                   <img src="http://picsum.photos/50" class="hover:brightness-75 w-14 h-14 rounded-full "/>
+                    <div>
+                        <div class="flex flex-col pl-2">
+                            <div class="text-xl font-semibold">jihyun.com</div> 
+                            <div>@jihyun</div>
+                        </div>
+                    </div> 
+                </div>
+                <i class="fa-solid fa-circle-info text-4xl pt-2 text-twitter opacity-60"></i>
+            
+            </div>
+            <!-- messager info -->
+            <div class="border-b border-gray hover:bg-gray hover:bg-opacity-40">
+                <div class="flex flex-col items-center pt-10 pb-4">
+                    <div class="flex space-x-2">
+                        <span class="font-bold">jihyun.com</span>
+                        <span>@jihyun</span>
+                    </div>
+                    <div class="flex space-x-2">
+                        <div class="flex space-x-1">
+                            <span class="font-semibold">28</span>
+                            <span>Following</span>
+                        </div>
+                        <div class="flex space-x-1">
+                            <span class="font-semibold">7</span>
+                            <span>Followers</span>
+                        </div>
+                    </div>
+                    <div class="flex space-x-1">
+                        <i class="fa-solid fa-calendar-days pt-1 pr-1"></i>
+                        <span>joined:</span>
+                        <span>September 2023</span>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- bubbles: me and opponent -->
+            <div class="flex-1" v-for="bubbles in 3">
+                <!-- chat bubble: me-->
+                <div class="flex flex-col items-end pt-5 pr-6">
+                    <div class="px-3 py-2 text-lg bg-twitter text-white rounded-ss-2xl rounded-se-2xl rounded-es-2xl cursor-pointer">What's up?</div>
+                    <div>3:45 PM</div>
+                </div>  
+                <!-- chat bubble: oppenent -->
+                <div class="flex flex-col items-start pl-6 pt-3" v-for="bubble in 3">
+                    <div class="px-3 py-2 text-lg bg-gray text-black rounded-ss-2xl rounded-se-2xl rounded-ee-2xl cursor-pointer">Hey! I'm sleeping</div>
+                    <div>4:00 PM</div>
+                </div>
+            </div>
+            
+            <!-- input -->
+            <div class="flex flex-row space-x-3 p-4 justify-center items-center sticky">
+                <i class="text-2xl text-twitter fa-regular fa-image"></i>
+                <i class="text-2xl text-twitter fa-solid fa-icons"></i>
+                <input placeholder="Start a new message" type="text" class="text-lg h-10 pl-2 w-full border-2 border-twitter focus:ring-1 focus:outline-none rounded-full"/>
+                <div>
+                    <button class="rounded-full hover:bg-twitter hover:bg-opacity-80 w-12 h-12 ml-0 bg-twitter bg-opacity-60 text-white font-bold">send</button>
+                </div>
+            </div>  
+           
+            
+        </div>
+        
+    </div>
 </div>
 </template>
 
